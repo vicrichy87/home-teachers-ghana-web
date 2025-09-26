1a94c35514e7f183f07543f97ae684a4f9c6817d
vicrichy87 <62028881+vicrichy87@users.noreply.github.com>
2025-09-26 07:48:17 +0100

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+// pages/_app.js
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
