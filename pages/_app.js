67afd58e428123de78f65c8c47d9ef9b157c4bb0
vicrichy87 <62028881+vicrichy87@users.noreply.github.com>
2025-09-28 09:24:56 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,22 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
 export default function MyApp({ Component, pageProps }) {
+  // create supabase client once per app load
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionContextProvider>
   );
 }
