0a0903ae6e009ce62e578b4a37b6c77edcdfe48c
vicrichy87 <62028881+vicrichy87@users.noreply.github.com>
2025-10-07 17:18:58 +0100

    Update teacher-student.js

--- pages/teacher-student.js
+++ pages/teacher-student.js
@@ -1,8 +1,8 @@
 // pages/teacher-student/[id].js
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { supabase } from "../../lib/supabaseClient";
-import Banner from "../../components/Banner";
+import { supabase } from "../lib/supabaseClient";
+import Banner from "../components/Banner";
 
 export default function TeacherStudentPage() {
   const router = useRouter();
