ee9a55ab1ea9dd04d13ddc036dd3f70dda3f0904
vicrichy87 <62028881+vicrichy87@users.noreply.github.com>
2025-10-07 18:04:11 +0100

    Update teacher.js

--- pages/teacher.js
+++ pages/teacher.js
@@ -578,7 +578,7 @@ export default function TeacherPage() {
                   <div
                     key={s.id}
                     onClick={() => router.push(
-                      `/pages/teacher-student.?teacher_id=${s.teacher_id}&student_id=${s.student.id}`
+                      `/pages/teacher-student?teacher_id=${s.teacher_id}&student_id=${s.student.id}`
                     )}
                     className="border p-3 rounded flex items-center gap-3 cursor-pointer hover:bg-gray-50 hover:shadow transition"
                   >
@@ -787,6 +787,7 @@ export default function TeacherPage() {
     </div>
   );
 }
+
 
 
 
