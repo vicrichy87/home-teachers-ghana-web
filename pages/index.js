57f9a280df7ebfd13671a25f9502b06af3aa7b4c
vicrichy87 <62028881+vicrichy87@users.noreply.github.com>
2025-10-20 09:15:29 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -233,7 +233,7 @@ export default function Home() {
             {isTeacher && (
               <div className="mb-4">
                 <label className="block mb-1 font-medium">
-                  Your Monthly Rate (GHC)
+                  Your Monthly Rate (USD)
                 </label>
                 <input
                   type="number"
